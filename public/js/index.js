f2c858f45f6a39d51131a8b400acc7ff3551565b
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-03-24 15:04:51 +0800

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,25 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit(
+    'createMessage',
+    {
+      from: 'User',
+      text: $('[name=message]').val()
+    },
+    function() {}
+  );
 });
